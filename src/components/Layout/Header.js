7e57a28c71899480f85ac641ea2ac451a07614e2
HabiburRahmanAnik <habib264521@gmail.com>
2022-06-04 19:53:52 +0600

    complete navbar

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,45 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://refertest.pythonanywhere.com/user/data')
+      .then(({ data }) => {
+        const user = data?.data;
+        setUserData(user);
+      });
+  }, []);
+
   return (
-    <div class="navbar bg-base-100">
-      <div class="flex-1">
-        <p class="btn btn-ghost normal-case text-xl">Job-Hunting</p>
+    <div className="navbar bg-base-100 shadow lg:px-32">
+      <div className="flex-1">
+        <p className="btn btn-ghost normal-case text-xl">Job-Hunting</p>
       </div>
-      <div class="flex-none">
-        <div class="dropdown dropdown-end">
-          <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-            <div class="w-10 rounded-full">
-              <img src="https://api.lorem.space/image/face?hash=33791" alt="" />
+      <div className="flex-none">
+        <div className="dropdown dropdown-end">
+          <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src={userData.pictureUrl} alt="" />
             </div>
           </label>
+          <ul
+            tabIndex="0"
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <p className='font-semibold'>{userData.name}</p>
+            </li>
+            <li>
+              <Link to="/">Profile</Link>
+            </li>
+            <li>
+              <Link to="/">Logout</Link>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
